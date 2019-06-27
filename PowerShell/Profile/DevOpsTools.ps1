867ccb31fe824a453934e317660c721d8bceec9e
Nate Ferrell <nate@scrthq.com>
2019-06-27 09:17:04 -0500

    updated DevOpsTools

--- PowerShell/Profile/DevOpsTools.ps1
+++ PowerShell/Profile/DevOpsTools.ps1
@@ -382,45 +382,52 @@ function global:Set-ProcessPriority {
 }
 
 function global:Update-GitAliases {
-    if (Get-Command git) {
-        $aliasList = @(
-            "a = !git add . && git status"
-            "aa = !git add . && git add -u . && git status"
-            "ac = !git add . && git commit"
-            "acm = !git add . && git commit -m"
-            "alias = !git config --get-regexp '^alias\.' | sort"
-            "amend = !git add -A && git commit --amend --no-edit"
-            "au = !git add -u . && git status"
-            "b = branch"
-            "ba = branch --all"
-            "c = commit"
-            "ca = commit --amend # careful"
-            "cam = commit -am"
-            "cm = commit -m"
-            "co = checkout"
-            "con = !git --no-pager config --list"
-            "conl = !git --no-pager config --local --list"
-            "conls = !git --no-pager config --local --list --show-origin"
-            "cons = !git --no-pager config --list --show-origin"
-            "current = !git branch | grep \* | cut -d ' ' -f2"
-            "d = !git --no-pager diff"
-            "f = fetch --all"
-            "fp = fetch --all --prune"
-            "l = log --graph --all --pretty=format:'%C(yellow)%h%C(cyan)%d%Creset %s %C(white)- %an, %ar%Creset'"
-            "lg = log --color --graph --pretty=format:'%C(bold white)%h%Creset -%C(bold green)%d%Creset %s %C(bold green)(%cr)%Creset %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-            "ll = log --stat --abbrev-commit"
-            "llg = log --color --graph --pretty=format:'%C(bold white)%H %d%Creset%n%s%n%+b%C(bold blue)%an <%ae>%Creset %C(bold green)%cr (%ci)' --abbrev-commit"
-            "master = checkout master"
-            "n = checkout -b"
-            "p = !git push"
-            "pf = !git push -f"
-            "pu = !git push -u origin ```$(git branch | grep \* | cut -d ' ' -f2)"
-            "s = status"
-            "ss = status -s"
-        )
-        foreach ($alias in $aliasList) {
-            $side = $alias -split " = "
-            Invoke-Expression $("git config --global alias.{0} `"{1}`"" -f $side[0],$side[1])
+    [CmdletBinding()]
+    Param()
+    Process {
+        if (Get-Command git) {
+            $aliasList = @(
+                "a = !git add . && git status"
+                "aa = !git add . && git add -u . && git status"
+                "ac = !git add . && git commit"
+                "acm = !git add . && git commit -m"
+                "alias = !git config --get-regexp '^alias\.' | sort"
+                "amend = !git add -A && git commit --amend --no-edit"
+                "au = !git add -u . && git status"
+                "b = branch"
+                "ba = branch --all"
+                "c = commit"
+                "ca = commit --amend # careful"
+                "cam = commit -am"
+                "cm = commit -m"
+                "co = checkout"
+                "con = !git --no-pager config --list"
+                "conl = !git --no-pager config --local --list"
+                "conls = !git --no-pager config --local --list --show-origin"
+                "cons = !git --no-pager config --list --show-origin"
+                "current = !git branch | grep \* | cut -d ' ' -f2"
+                "d = !git --no-pager diff"
+                "f = fetch --all"
+                "fp = fetch --all --prune"
+                "l = log --graph --all --pretty=format:'%C(yellow)%h%C(cyan)%d%Creset %s %C(white)- %an, %ar%Creset'"
+                "lg = log --color --graph --pretty=format:'%C(bold white)%h%Creset -%C(bold green)%d%Creset %s %C(bold green)(%cr)%Creset %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+                "ll = log --stat --abbrev-commit"
+                "llg = log --color --graph --pretty=format:'%C(bold white)%H %d%Creset%n%s%n%+b%C(bold blue)%an <%ae>%Creset %C(bold green)%cr (%ci)' --abbrev-commit"
+                "master = checkout master"
+                "n = checkout -b"
+                "origin = remote get-url origin"
+                "p = !git push"
+                "pf = !git push -f"
+                "pu = !git push -u origin ```$(git branch | grep \* | cut -d ' ' -f2)"
+                "s = status"
+                "ss = status -s"
+            )
+            Write-Verbose "Setting git aliases:"
+            foreach ($alias in $aliasList) {
+                $side = $alias -split " = "
+                Write-Verbose "$($side[0]) = $($side[1])"
+                Invoke-Expression $("git config --global alias.{0} `"{1}`"" -f $side[0],$side[1])
+            }
         }
     }
 }
