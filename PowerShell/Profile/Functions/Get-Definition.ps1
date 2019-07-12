70f1d100152839f61efe392d5fc1adc9493eac95
Nate Ferrell <nate@scrthq.com>
2019-07-11 23:44:33 -0500

    updated prompt and stuff

---
+++ PowerShell/Profile/Functions/Get-Definition.ps1
@@ -0,0 +1,28 @@
+function Get-Definition {
+    [CmdletBinding()]
+    Param(
+        [parameter(Mandatory,Position = 0)]
+        [String]
+        $Command
+    )
+    Process {
+        try {
+            $Defintion = (Get-Command $Command -ErrorAction Stop).Definition
+            "function $Command {$Defintion}"
+        }
+        catch {
+            throw
+        }
+    }
+}
+
+'def','Define' | ForEach-Object {
+    Set-Alias -Name $_ -Value Get-Definition -Option AllScope -Scope Global
+}
+
+Register-ArgumentCompleter -CommandName 'Get-Definition' -ParameterName 'Command' -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+    (Get-Command "$wordToComplete*").Name | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
